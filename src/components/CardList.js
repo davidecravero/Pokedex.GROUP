77494e5e88a7c3b587519a988fad61602a19ff0a
Natascha <NataschaHof@gmx.de>
2020-09-02 21:40:58 +0200

    Updates Stats and Sprites

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,34 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./../css/CardList.css";
-import Stats from "./Stats";
-
-const highResImageURL="https://pokeres.bastionbot.org/images/pokemon/[id].png";
-
-
-// get pokemon id from details url (index doesn't match pokemon number)
-const HQimg = (name, id) => {
-  let result=<img className="poke-image" src={highResImageURL.replace("[id]",id)} alt={"HQ image of " + name} />
-  return result;
-}
-// get pokemon id from details url (index doesn't match pokemon number)
-const extractID = (pokeURL) => {
-  // parse whether url ends on / and remove it if it does
-  let result=pokeURL.slice(-1)==="/"?pokeURL.substring(0,pokeURL.length-1):pokeURL;
-  // now find last / before id 
-  result=result.substring(result.lastIndexOf("/")+1);
-  return result;
-}
 
 const CardList = (props) => {
-  const id=extractID(props.data.url);
   return (
     <div className="poke-card-list">
-      <h2>{props.data.name}</h2>
-      <h3>#{id}</h3>
-      {HQimg(props.data.name, id)}
+      <h1>{props.data.name}</h1>
       <Link to={`/detail/${props.data.name}`}>View Details</Link>
-      <Stats />
     </div>
   );
 };
