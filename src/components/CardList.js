94a6e0d151809730cf43fdad7577c4628e3f24ca
Scirius <68830011+Scirius@users.noreply.github.com>
2020-09-02 14:35:34 +0200

    Added high-res images to list

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,6 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./../css/CardList.css";
 
+const highResImageURL="https://pokeres.bastionbot.org/images/pokemon/[id].png";
+
+
+// get pokemon id from details url (index doesn't match pokemon number)
+const HQimg = (id) => {
+  let result=<img className="poke-image" src={highResImageURL.replace("[id]",id)} />
+  return result;
+}
 // get pokemon id from details url (index doesn't match pokemon number)
 const extractID = (pokeURL) => {
   // parse whether url ends on / and remove it if it does
@@ -12,10 +20,12 @@ const extractID = (pokeURL) => {
 }
 
 const CardList = (props) => {
+  const id=extractID(props.data.url);
   return (
     <div className="poke-card-list">
       <h2>{props.data.name}</h2>
-      <h3>#{extractID(props.data.url)}</h3>
+      <h3>#{id}</h3>
+      {HQimg(id)}
       <Link to={`/detail/${props.data.name}`}>View Details</Link>
     </div>
   );
