96af6bc663b2e719430d8045ca94bcfda3f74f9d
Natascha <NataschaHof@gmx.de>
2020-09-04 18:00:47 +0200

    NavBar & Routing BerriesView

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,25 +2,24 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./../css/CardList.css";
 
-const highResImageURL="https://pokeres.bastionbot.org/images/pokemon/[id].png";
-
+const highResImageURL = "https://pokeres.bastionbot.org/images/pokemon/[id].png";
 
 // get pokemon id from details url (index doesn't match pokemon number)
 const HQimg = (name, id) => {
-  let result=<img className="poke-image" src={highResImageURL.replace("[id]",id)} alt={"HQ image of " + name} title={"HQ image of " + name} />
+  let result = <img className="poke-image" src={highResImageURL.replace("[id]", id)} alt={"HQ image of " + name} title={"HQ image of " + name} />;
   return result;
-}
+};
 // get pokemon id from details url (index doesn't match pokemon number)
 const extractID = (pokeURL) => {
   // parse whether url ends on / and remove it if it does
-  let result=pokeURL.slice(-1)==="/"?pokeURL.substring(0,pokeURL.length-1):pokeURL;
-  // now find last / before id 
-  result=result.substring(result.lastIndexOf("/")+1);
+  let result = pokeURL.slice(-1) === "/" ? pokeURL.substring(0, pokeURL.length - 1) : pokeURL;
+  // now find last / before id
+  result = result.substring(result.lastIndexOf("/") + 1);
   return result;
-}
+};
 
 const CardList = (props) => {
-  const id=extractID(props.data.url);
+  const id = extractID(props.data.url);
   return (
     <div className="poke-card-list">
       <h2>{props.data.name}</h2>
